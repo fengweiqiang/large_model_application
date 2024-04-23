4033585cf436d4e71dcf1efac76bf91822b8f5c0
fengweiqiang <550838476@qq.com>
2024-04-23 18:07:33 +0800

    golang 使用gin+langchaingo调用llm模型demo

--- router/router.go
+++ router/router.go
@@ -10,7 +10,6 @@ func RunServer(port string) {
 	group := r.Group("/app/v1")
 	group.POST("/question", controller.QuestionController{}.Question)
 	group.GET("/questionStream", controller.QuestionController{}.QuestionStream)
-	group.GET("/questionStream2", controller.QuestionController{}.QuestionStream2)
 	err := r.Run(":" + port)
 	if err != nil {
 		panic(err)
